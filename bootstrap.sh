c9aae4cbf15555c111ff939f16d258176e97590c
Makoto Katsumata <mako10k@mk10.org>
2024-12-06 11:24:30 +0000

    improve bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
-export CC=clang
+basedir="$(dirname "$0")"
 user=$(id -nu)
 group=$(id -ng)
 
 set -ex
 
-sudo chown -R $user:$group "$(dirname "$0")"
+sudo chown -R $user:$group "$basedir"
+
+if [ -z "${CC}" ]; then
+    if clang -v > /dev/null 2>&1; then
+        CC=clang
+    else
+        CC=gcc
+    fi
+fi
+
+if [ -f "$basedir/Makefile" ]; then
+    make distclean || true
+fi
 
 autoreconf -fiv
-./configure --enable-debug
+./configure CC="$CC" --enable-debug
 make clean all check
 sudo make install
